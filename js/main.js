ac1787363264ddc42ab24cd7ab9b76442746cdb5
eleanorcode <innocence17@gmail.com>
2023-05-23 17:29:26 +0300

    Add source maps

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // import 'bootstrap.bundle.min.js'
-import './bootstrap.bundle.min.js'
+import "./bootstrap.bundle.min.js";
 
 // initialize all popovers
 document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
-  new bootstrap.Popover(popover)
-})
+	new bootstrap.Popover(popover);
+});
+
+sourceMappingURL = bootstrap.bundle.min.js.map;
