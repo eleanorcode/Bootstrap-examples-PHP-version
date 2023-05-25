6277e3d9b81792eef5a25a15843835eb3237c885
eleanorcode <innocence17@gmail.com>
2023-05-25 11:48:42 +0300

    Update examples

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ if (isset($_GET['template'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="assets/img/favicons/favicon.ico">
+  <link rel="apple-touch-icon" href="assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+  <link rel="icon" href="assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+  <link rel="icon" href="assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+  <link rel="manifest" href="assets/img/favicons/manifest.json">
+  <link rel="mask-icon" href="assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
   <link rel="stylesheet" href="css/styles.css">
   <?php
 if (isset($template)) {
@@ -27,6 +33,7 @@ if (isset($template)) {
 if (!isset($template)) {
   echo file_get_contents("home.html");
 } else {
+  echo file_get_contents("mode-toggler.html");
   echo file_get_contents("examples/$template/index.html");
 }?>
 
