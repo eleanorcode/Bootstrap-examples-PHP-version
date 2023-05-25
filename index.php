f5f13e7643636f994cf1acdff944a87145979292
eleanorcode <innocence17@gmail.com>
2023-05-25 18:40:02 +0300

    Fix url

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (isset($_GET['template'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="assets/img/favicons/favicon.ico">
+  <link rel="icon" href="assets/favicons/favicon.ico">
   <link rel="icon" href="assets/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
   <link rel="icon" href="assets/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
   <link rel="apple-touch-icon" href="assets/favicons/apple-touch-icon.png" sizes="180x180">
