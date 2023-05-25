c59711e458211ff170a6ce96068d8495239b2e19
eleanorcode <innocence17@gmail.com>
2023-05-25 18:33:30 +0300

    Move favicons

--- index.php
+++ index.php
@@ -10,11 +10,11 @@ if (isset($_GET['template'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="assets/img/favicons/favicon.ico">
-  <link rel="apple-touch-icon" href="assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-  <link rel="icon" href="assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-  <link rel="icon" href="assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-  <link rel="manifest" href="assets/img/favicons/manifest.json">
-  <link rel="mask-icon" href="assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
+  <link rel="icon" href="assets/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+  <link rel="icon" href="assets/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+  <link rel="apple-touch-icon" href="assets/favicons/apple-touch-icon.png" sizes="180x180">
+  <link rel="mask-icon" href="assets/favicons/safari-pinned-tab.svg" color="#712cf9">
+  <link rel="manifest" href="assets/favicons/manifest.json">
   <link rel="stylesheet" href="css/styles.css">
   <?php
 if (isset($template)) {
