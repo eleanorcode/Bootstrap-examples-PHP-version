aeb8d090c58dc87beb2515399a7c84def5214485
eleanorcode <innocence17@gmail.com>
2023-06-01 22:57:50 +0300

    Upgrade for Bootstrap 5.3.0

--- index.php
+++ index.php
@@ -47,10 +47,7 @@ if (!isset($template)) {
 if (isset($template)) {
   echo "<script src='js/color-modes.js'></script>";
   if ($template === 'dashboard') {
-    echo '<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-  integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>';
-    echo '<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
-  integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>';
+    echo '<script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js" integrity="sha384-gdQErvCNWvHQZj6XZM0dNsAoY4v+j5P1XDpNkcM3HJG1Yx04ecqIHk7+4VBOCHOG" crossorigin="anonymous"></script>';
   }
   if ($template === 'masonry') {
     echo '<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
